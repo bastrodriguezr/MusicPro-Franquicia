048078ea7f054ee11ffb0bb5e0f4bf00273fe21a
bastrodriguezr <bast.rodriguezr@gmail.com>
2023-05-19 10:03:18 -0400

    Updated Path MPFranquicia/plantillas

--- MPFranquicia/settings.py
+++ MPFranquicia/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "MPFranquicia.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['MPFranquicia\plantillas'],
+        "DIRS": ['MPFranquicia/plantillas'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
