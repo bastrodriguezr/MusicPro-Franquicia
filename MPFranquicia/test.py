751f12f1b07368eec31811351b2b328223fd11b1
bastrodriguezr <bast.rodriguezr@gmail.com>
2023-06-24 22:51:57 -0400

    Archivo de Test Modificado

--- MPFranquicia/test.py
+++ MPFranquicia/test.py
@@ -2,11 +2,22 @@ from django.test import TestCase
 from faker import Faker
 from MusicPro.models import Producto
 
+from django.core.files.uploadedfile import SimpleUploadedFile
+from faker import Faker
+from PIL import Image
+from io import BytesIO
+
 class ProductoTestCase(TestCase):
 
     def test_carga_1000_productos(self):
         fake = Faker()
         cantidad = 1000
+
+        image = Image.new('RGB', (200, 200))
+        file_name = fake.file_name(extension='jpg')
+        image_io = BytesIO()
+        image.save(image_io, 'JPEG')
+        image_file = SimpleUploadedFile(file_name, image_io.getvalue(), content_type='image/jpeg')
         
         for _ in range(cantidad):
             producto = Producto.objects.create(
@@ -23,6 +34,12 @@ class ProductoTestCase(TestCase):
     def test_carga_10000_productos(self):
         fake = Faker()
         cantidad = 10000
+
+        image = Image.new('RGB', (200, 200))
+        file_name = fake.file_name(extension='jpg')
+        image_io = BytesIO()
+        image.save(image_io, 'JPEG')
+        image_file = SimpleUploadedFile(file_name, image_io.getvalue(), content_type='image/jpeg')
         
         for _ in range(cantidad):
             producto = Producto.objects.create(
@@ -39,6 +56,12 @@ class ProductoTestCase(TestCase):
     def test_carga_100000_productos(self):
         fake = Faker()
         cantidad = 100000
+
+        image = Image.new('RGB', (200, 200))
+        file_name = fake.file_name(extension='jpg')
+        image_io = BytesIO()
+        image.save(image_io, 'JPEG')
+        image_file = SimpleUploadedFile(file_name, image_io.getvalue(), content_type='image/jpeg')
         
         for _ in range(cantidad):
             producto = Producto.objects.create(
